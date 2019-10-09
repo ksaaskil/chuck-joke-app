dbae3121054fe4fbb7dd140392502077a2adef64
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-09 16:06:52 +0300

    Setup enzyme.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,18 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App';
+import ReactTestUtils from 'react-dom/test-utils'; // ES6
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {mount, shallow} from 'enzyme';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', done => {
+  // global.fetch = jest.fn(() => Promise.resolve());
+  let wrapper;
+  ReactTestUtils.act(() => {
+    wrapper = mount(<App />);
+  });
+  console.log(wrapper);
+  done();
 });
